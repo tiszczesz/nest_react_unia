24d78813b06ae8106494e08d72983318ebc2999c
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-03-14 14:17:38 +0100

    kk

--- cw3/src/router.ts
+++ cw3/src/router.ts
@@ -1,8 +1,12 @@
 import { Router, type Request,type Response } from "express";
 import fs from 'fs/promises';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 const router = Router();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathToPublic = path.join(__dirname, '..', 'public', 'index.html');
 
+console.log(pathToPublic);
 // Define a simple route
 router.get('/', (req: Request, res: Response) => {
     res.status(200).json({ message: 'Welcome to the Express server!' });
@@ -11,7 +15,7 @@ router.get('/info', (req: Request, res: Response) => {
     res.status(200).json({ message: 'Info from the server!' });
 });
 router.get('/index', async (req: Request, res: Response) => {
-    const context = await fs.readFile('./public/index.html');
+    const context = await fs.readFile(pathToPublic, 'utf-8');
     res.status(200).send(context);
 });
 export default router;
