89bb2149ee088b5f85f212a98c7c8009a6a041da
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-03-14 14:27:03 +0100

    kk

--- cw3/src/router.ts
+++ cw3/src/router.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 const router = Router();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const pathToPublic = path.join(__dirname, '..', 'public', 'index.html');
+const pathToPublic = path.join(__dirname, '..', 'public');
 
 console.log(pathToPublic);
 // Define a simple route
@@ -15,7 +15,11 @@ router.get('/info', (req: Request, res: Response) => {
     res.status(200).json({ message: 'Info from the server!' });
 });
 router.get('/index', async (req: Request, res: Response) => {
-    const context = await fs.readFile(pathToPublic, 'utf-8');
+    const context = await fs.readFile(path.join(pathToPublic, 'index.html'), 'utf-8');
     res.status(200).send(context);
+});
+router.get('/students', async (req: Request, res: Response) => {
+    const students = await fs.readFile(path.join(pathToPublic, 'students.json'), 'utf-8');
+    res.status(200).json(JSON.parse(students));
 });
 export default router;
