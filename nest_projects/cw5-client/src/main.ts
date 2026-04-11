40915d493e463ec28d282b1d083a7c6946af04a0
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-04-11 11:46:37 +0200

    hh

--- nest_projects/cw5-client/src/main.ts
+++ nest_projects/cw5-client/src/main.ts
@@ -3,9 +3,16 @@ const app = document.querySelector<HTMLDivElement>('#app');
 const buttonLoad = document.createElement('button');
 buttonLoad.classList.add('btn', 'btn-outline-primary');
 buttonLoad.textContent = 'Załaduj dane';
+const sectionResult = document.createElement('section');
+sectionResult.id = 'sectionResult';
+buttonLoad.addEventListener('click', async () => {
+  console.log('Ładowanie danych...');
+  sectionResult.textContent = 'Ładowanie danych...';
+});
 if(app) {
   app.classList.add('container');
   app.innerHTML = `
     <h1>Hello Vite + TypeScript!</h1>`;
   app.appendChild(buttonLoad);
+  app.appendChild(sectionResult);
 }
