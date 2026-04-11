540e9163098f998ec7dee2c9625d4ec19799526c
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-04-11 09:22:09 +0200

    kk

--- nest_projects/cw5/src/models/MysqlProductRepo.ts
+++ nest_projects/cw5/src/models/MysqlProductRepo.ts
@@ -3,7 +3,7 @@ import { Product } from './productModel';
 import mysql from 'mysql2/promise';
 
 class MysqlProductRepo implements IProductRepo {
-  private connection: any; // Replace with actual MySQL connection type
+  private connection: mysql.Connection | null; // Replace with actual MySQL connection type
 
   private async createNewConnectio() {
     return await mysql.createConnection({
@@ -13,8 +13,29 @@ class MysqlProductRepo implements IProductRepo {
       database: 'nest_products',
     });
   }
-  getProducts(): Promise<Product[]> {
-    throw new Error('Method not implemented.');
+  private async getConnection() {
+    if (!this.connection) {
+      this.connection = await this.createNewConnectio();
+    }
+    return this.connection;
+  }
+  /**
+   *
+   */
+  constructor() {
+    this.connection = null;
+  }
+  async getProducts(): Promise<Product[]> {
+    //Tworzymy nowe połączenie do bazy danych
+    await this.getConnection();
+    //Jeśli połączenie nie zostało nawiązane, zwracamy pustą tablicę
+    if (!this.connection) return [];
+    //Wykonujemy zapytanie do bazy danych, aby pobrać wszystkie produkty
+    const [rows] = await this.connection.execute('SELECT * FROM products');
+    //Zamykamy połączenie z bazą danych
+    await this.connection.end();
+    //Zwracamy pobrane produkty jako tablicę obiektów typu Product
+    return rows as Product[];
   }
   getProductById(id: number): Promise<Product | null> {
     throw new Error('Method not implemented.');
