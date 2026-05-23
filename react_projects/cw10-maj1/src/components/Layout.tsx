711f9c88669ff1adabea538460275e9767405498
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-05-23 09:56:04 +0200

    kk

--- react_projects/cw10-maj1/src/components/Layout.tsx
+++ react_projects/cw10-maj1/src/components/Layout.tsx
@@ -5,9 +5,9 @@ const Layout = () => {
     return (
         <div>
             <nav className="d-flex  gap-3 p-3 bg-light">
-                <NavLink to="/" className={({ isActive }) => isActive ? "text-primary" : ""}>Home</NavLink>
-                <NavLink to="/about" className={({ isActive }) => isActive ? "text-primary" : ""}>About</NavLink>
-                <NavLink to="/contact" className={({ isActive }) => isActive ? "text-primary" : ""}>Contact</NavLink>
+                <NavLink to="/" className="myNav-link">Home</NavLink>
+                <NavLink to="/about" className="myNav-link">About</NavLink>
+                <NavLink to="/contact" className="myNav-link">Contact</NavLink>
 
             </nav>
             <main>
