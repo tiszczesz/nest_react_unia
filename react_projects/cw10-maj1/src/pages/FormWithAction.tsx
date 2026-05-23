1bf6217ebe4b2aa9be1d07242d3d98339112ef54
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-05-23 11:50:59 +0200

    kk

--- react_projects/cw10-maj1/src/pages/FormWithAction.tsx
+++ react_projects/cw10-maj1/src/pages/FormWithAction.tsx
@@ -3,21 +3,29 @@ import { useState } from "react";
 
 const FormWithAction = () => {
     const [result, setResult] = useState("");
-  return (
-    <div>
-        <h2>Form with Action</h2>
-         <form >
+
+    function handleAction(formDate: FormData): void | Promise<void> {
+        console.log('handle action')
+        setResult(`Dane z formularza: imię: ${formDate.get("firstname")}, 
+                    data urodzenia: ${formDate.get("birthDate")},
+                   wykształcenie: ${formDate.get("education")}`);
+    }
+
+    return (
+        <div>
+            <h2>Form with Action</h2>
+            <form action={(fd) => handleAction(fd)} >
                 <div className="row m-2">
                     <label className="col-3 text-end" htmlFor="firstname">Podaj imię:</label>
-                    <input  className="col-6" type="text" id="firstname" />
+                    <input className="col-6" type="text" id="firstname" name="firstname" />
                 </div>
                 <div className="row m-2">
                     <label className="col-3 text-end" htmlFor="birthDate">Podaj datę urodzenia:</label>
-                    <input  className="col-6" type="date" id="birthDate" />
+                    <input className="col-6" type="date" id="birthDate" name="birthDate" />
                 </div>
                 <div className="row m-2">
                     <label className="col-3 text-end" htmlFor="education">Podaj poziom wykształcenia:</label>
-                    <select  className="col-6" id="education">
+                    <select className="col-6" id="education" name="education">
                         <option value="podstawowe">podstawowe</option>
                         <option value="średnie">średnie</option>
                         <option value="wyższe">wyższe</option>
@@ -31,7 +39,7 @@ const FormWithAction = () => {
             </form>
             <hr />
             <div className="border p-2">{result}</div></div>
-  )
+    )
 }
 
 export default FormWithAction
--- react_projects/cw10-maj1/src/pages/FormWithRef.tsx
+++ react_projects/cw10-maj1/src/pages/FormWithRef.tsx
@@ -1,23 +1,38 @@
-import { useState } from "react";
+import { useRef, useState, type SubmitEvent } from "react";
 
 
 const FormWithRef = () => {
+    console.log('render form with ref')
     const [result, setResult] = useState("");
+    const firstnameRef = useRef<HTMLInputElement>(null);
+    const birthDateRef = useRef<HTMLInputElement>(null);
+    const educationRef = useRef<HTMLSelectElement>(null);
+    function handleSubmit(e: SubmitEvent<HTMLFormElement>): void {
+        //throw new Error("Function not implemented.");
+        e.preventDefault();
+        if (firstnameRef.current && birthDateRef.current && educationRef.current) {
+            const firstname = firstnameRef.current.value;
+            const birthDate = birthDateRef.current.value;
+            const education = educationRef.current.value;
+            setResult(`Dane z formularza: imię: ${firstname}, data urodzenia: ${birthDate}, wykształcenie: ${education}`);
+        }
+    }
+
     return (
         <div>
             <h2>Form with Ref</h2>
-            <form >
+            <form onSubmit={(e) => handleSubmit(e)} >
                 <div className="row m-2">
                     <label className="col-3 text-end" htmlFor="firstname">Podaj imię:</label>
-                    <input className="col-6" type="text" id="firstname" />
+                    <input ref={firstnameRef} className="col-6" type="text" id="firstname" />
                 </div>
                 <div className="row m-2">
                     <label className="col-3 text-end" htmlFor="birthDate">Podaj datę urodzenia:</label>
-                    <input className="col-6" type="date" id="birthDate" />
+                    <input ref={birthDateRef} className="col-6" type="date" id="birthDate" />
                 </div>
                 <div className="row m-2">
                     <label className="col-3 text-end" htmlFor="education">Podaj poziom wykształcenia:</label>
-                    <select className="col-6" id="education">
+                    <select ref={educationRef} className="col-6" id="education">
                         <option value="podstawowe">podstawowe</option>
                         <option value="średnie">średnie</option>
                         <option value="wyższe">wyższe</option>
