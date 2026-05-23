cf646c0c9004804c7f81544da04a587fa2241de6
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-05-23 10:50:48 +0200

    kk

--- react_projects/cw10-maj1/src/pages/FormWithAction.tsx
+++ react_projects/cw10-maj1/src/pages/FormWithAction.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import { useState } from "react";
 
-type Props = {}
 
-const FormWithAction = (props: Props) => {
+const FormWithAction = () => {
+    const [result, setResult] = useState("");
   return (
-    <div>FormWithAction</div>
+    <div> <form >
+                <div className="row m-2">
+                    <label className="col-3 text-end" htmlFor="firstname">Podaj imię:</label>
+                    <input  className="col-6" type="text" id="firstname" />
+                </div>
+                <div className="row m-2">
+                    <label className="col-3 text-end" htmlFor="birthDate">Podaj datę urodzenia:</label>
+                    <input  className="col-6" type="date" id="birthDate" />
+                </div>
+                <div className="row m-2">
+                    <label className="col-3 text-end" htmlFor="education">Podaj poziom wykształcenia:</label>
+                    <select  className="col-6" id="education">
+                        <option value="podstawowe">podstawowe</option>
+                        <option value="średnie">średnie</option>
+                        <option value="wyższe">wyższe</option>
+                    </select>
+                </div>
+                <div className="row m-2">
+                    <button className="col-6 offset-3 btn btn-outline-primary" type="submit">
+                        Wyślij
+                    </button>
+                </div>
+            </form>
+            <hr />
+            <div className="border p-2">{result}</div></div>
   )
 }
 
