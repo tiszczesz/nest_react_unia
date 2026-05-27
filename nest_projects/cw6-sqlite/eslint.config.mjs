097fcf72b8413c719d8e1aa98302dd97491bd4cc
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-05-27 13:53:47 +0200

    kk

--- nest_projects/cw6-sqlite/eslint.config.mjs
+++ nest_projects/cw6-sqlite/eslint.config.mjs
@@ -27,7 +27,8 @@ export default tseslint.config(
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unsafe-call': 'warn',
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       "prettier/prettier": ["error", { endOfLine: "auto" }],
